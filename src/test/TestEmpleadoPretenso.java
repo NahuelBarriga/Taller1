43de7efaf1c14d75935fd071a5502e8abe1ecaa7
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-11-03 17:09:14 -0300

    ad

--- src/test/TestEmpleadoPretenso.java
+++ src/test/TestEmpleadoPretenso.java
@@ -6,17 +6,20 @@ import org.junit.Test;
 
 import modeloDatos.EmpleadoPretenso;
 
-public class TestEmpleadoPretenso {
 
+
+public class TestEmpleadoPretenso {
+	
 	@Test
 	public void testEmpleadoPretenso() {
 		EmpleadoPretenso empleadoTest = new EmpleadoPretenso("Juan123", "Juan123", "Juan", "2235698547", "Rodriguez", 25); 
-		
-		Assert.assertNotNull("TaBien", empleadoTest);
+		int Edad = empleadoTest.getEdad();
+		Assert.assertEquals(Edad, 25);
+		//Assert.assertNotNull("TaBien", empleadoTest);
 	}
 
 	@Test
-	public void testSetEdad() {
+	public void testSetEdad() { 
 		
 	}
 
---
+++ src/test/TestTicket.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import modeloDatos.Ticket;
+
+public class TestTicket {
+
+	@Test
+	public void testTicketStringIntStringStringStringString() {
+		Ticket ticketTest = new Ticket();
+	}
+
+	@Test
+	public void testSetRemuneracion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComparacionLocacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComparacionJornada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComparacionPuesto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComparacionEstudios() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComparacionExperiencia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComparacionRemuneracion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComparacionTotal() {
+		fail("Not yet implemented");
+	}
+
+}
