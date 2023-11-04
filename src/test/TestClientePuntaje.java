ddc4b5a518a823444d8925d945473b44d0f215d0
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-11-04 18:03:49 -0300

    libertadores

---
+++ src/test/TestClientePuntaje.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestClientePuntaje {
+
+	@Test
+	public void testClientePuntajeDoubleCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCliente() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/TestUsuarioPuntaje.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUsuarioPuntaje {
+
+	@Test
+	public void testUsuarioPuntaje() {
+		
+	}
+
+	@Test
+	public void testSetUsuario() {
+		fail("Not yet implemented");
+	}
+
+}
