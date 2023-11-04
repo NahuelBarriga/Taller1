8c512d97164be7e1824ddea0e82bc23285bbb5b1
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-11-04 13:12:04 -0300

    adw

---
+++ src/test/TestContratacion.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestContratacion {
+
+	@Test
+	public void testContratacionEmpleadorEmpleadoPretenso() {
+		Empleado empleadoTest = new Empleado();
+		
+		fail("Not yet implemented");
+	}
+
+}
--- src/test/TestTicket.java
+++ src/test/TestTicket.java
@@ -68,42 +68,219 @@ public class TestTicket {
 
 	@Test
 	public void testSetRemuneracion() {
-		fail("Not yet implemented");
+		Ticket testTicket = new Ticket("HOME_OFFICE", 1200, "JORNADA_COMPLETA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		testTicket.setRemuneracion(5200);
+		int remuneracionTest = 5200; 
+		Assert.assertEquals(testTicket.getRemuneracion(), remuneracionTest);
+		
+		
 	}
 
 	@Test
+	//asumo que esta mal la forma en lo que lo hice pq si fuese asi y tengo un error no tengo idea de donde carajo salio
 	public void testGetComparacionLocacion() {
-		fail("Not yet implemented");
+		Ticket testTicketPresencial= new Ticket("PRESENCIAL", 1200, "JORNADA_COMPLETA", "JUNIOR", "EXP_NADA", "PRIMARIOS"); 
+		Ticket testTicketHomeOffice = new Ticket("HOME_OFFICE", 1200, "JORNADA_COMPLETA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketIndiferente = new Ticket("INDIFERENTE", 1200, "JORNADA_COMPLETA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		double ResultadoLocacionTest1 = 1;
+		double ResultadoLocacionTest2 = -1;
+		double resultado;
+		//Escenario 1, this.ticket.locacion == PRESENCIAL: 
+		resultado = testTicketPresencial.getComparacionLocacion(testTicketPresencial);
+		Assert.assertEquals(ResultadoLocacionTest1, resultado, 0); 
+		resultado = testTicketPresencial.getComparacionLocacion(testTicketHomeOffice);
+		Assert.assertEquals(ResultadoLocacionTest2, resultado, 0); 
+		resultado = testTicketPresencial.getComparacionLocacion(testTicketIndiferente);
+		Assert.assertEquals(ResultadoLocacionTest2, resultado, 0); 
+		
+		/*//Escenario 2, this.ticket.locacion == HOME_OFFICE:
+		
+		resultado = testTicketHomeOffice.getComparacionLocacion(testTicketPresencial);
+		Assert.assertEquals(ResultadoLocacionTest2, resultado, 0);
+		resultado = testTicketHomeOffice.getComparacionLocacion(testTicketHomeOffice);
+		Assert.assertEquals(ResultadoLocacionTest1, resultado, 0);
+		resultado = testTicketHomeOffice.getComparacionLocacion(testTicketIndiferente);
+		Assert.assertEquals(ResultadoLocacionTest1, resultado, 0);
+		
+		//Escenario 3, this.ticket.locacion == INDIFERENTE:
+		resultado = testTicketIndiferente.getComparacionLocacion(testTicketPresencial);
+		Assert.assertEquals(ResultadoLocacionTest2, resultado, 0);
+		resultado = testTicketIndiferente.getComparacionLocacion(testTicketHomeOffice);
+		Assert.assertEquals(ResultadoLocacionTest1, resultado, 0);
+		resultado = testTicketIndiferente.getComparacionLocacion(testTicketIndiferente);
+		Assert.assertEquals(ResultadoLocacionTest1, resultado, 0);*/
 	}
 
 	@Test
 	public void testGetComparacionJornada() {
-		fail("Not yet implemented");
+		Ticket testTicketJornadaMedia= new Ticket("PRESENCIAL", 1200, "JORNADA_MEDIA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketJornadaCompleta= new Ticket("PRESENCIAL", 1200, "JORNADA_COMPLETA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketJornadaExtendida= new Ticket("PRESENCIAL", 1200, "JORNADA_EXTENDIDA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		double resultadoTest1 = 1;
+		double resultadoTest2 = -1;
+		double resultadoTest3 = -0.5;
+		double resultado;
+		//Escenario 1: (this.jornada== JORNADA_"MEDIA")				
+		resultado = testTicketJornadaMedia.getComparacionJornada(testTicketJornadaMedia);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketJornadaMedia.getComparacionJornada(testTicketJornadaCompleta);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+		resultado = testTicketJornadaMedia.getComparacionJornada(testTicketJornadaExtendida);
+		Assert.assertEquals(resultado, resultadoTest2, 0);
+		
+		//Escenario 2: (this.jornada== JORNADA_"COMPLETA")				
+		resultado = testTicketJornadaCompleta.getComparacionJornada(testTicketJornadaMedia);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+		resultado = testTicketJornadaCompleta.getComparacionJornada(testTicketJornadaCompleta);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketJornadaCompleta.getComparacionJornada(testTicketJornadaExtendida);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+		
+		//Escenario 3: (this.jornada== JORNADA_"EXTENDIDA")				
+		resultado = testTicketJornadaExtendida.getComparacionJornada(testTicketJornadaMedia);
+		Assert.assertEquals(resultado, resultadoTest2, 0);
+		resultado = testTicketJornadaExtendida.getComparacionJornada(testTicketJornadaCompleta);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketJornadaExtendida.getComparacionJornada(testTicketJornadaExtendida);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
 	}
 
 	@Test
 	public void testGetComparacionPuesto() {
-		fail("Not yet implemented");
+		Ticket testTicketJunior= new Ticket("PRESENCIAL", 1200, "JORNADA_MEDIA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketSenior= new Ticket("PRESENCIAL", 1200, "JORNADA_COMPLETA", "SENIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketManagment= new Ticket("PRESENCIAL", 1200, "JORNADA_EXTENDIDA", "MANAGMENT", "EXP_NADA", "PRIMARIOS");
+		double resultadoTest1 = 1;
+		double resultadoTest2 = -1;
+		double resultadoTest3 = -0.5;
+		double resultado;
+		
+		//Escenario 1: (this.puesto== "JUNIOR")
+		resultado = testTicketJunior.getComparacionPuesto(testTicketJunior);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketJunior.getComparacionPuesto(testTicketSenior);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+		resultado = testTicketJunior.getComparacionPuesto(testTicketManagment);
+		Assert.assertEquals(resultado, resultadoTest2, 0);
+		
+		//Escenario 2: (this.puesto== "SENIOR")
+		resultado = testTicketSenior.getComparacionPuesto(testTicketJunior);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+		resultado = testTicketSenior.getComparacionPuesto(testTicketSenior);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketSenior.getComparacionPuesto(testTicketManagment);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+	
+		//Escenario 3: (this.puesto== "MANAGMENT")			
+		resultado = testTicketManagment.getComparacionPuesto(testTicketJunior);
+		Assert.assertEquals(resultado, resultadoTest2, 0);
+		resultado = testTicketManagment.getComparacionPuesto(testTicketSenior);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketManagment.getComparacionPuesto(testTicketManagment);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
 	}
 
 	@Test
 	public void testGetComparacionEstudios() {
-		fail("Not yet implemented");
+		Ticket testTicketPrimarios= new Ticket("PRESENCIAL", 1200, "JORNADA_MEDIA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketSecundarios= new Ticket("PRESENCIAL", 1200, "JORNADA_COMPLETA", "SENIOR", "EXP_NADA", "SECUNDARIOS");
+		Ticket testTicketTerciarios= new Ticket("PRESENCIAL", 1200, "JORNADA_EXTENDIDA", "MANAGMENT", "EXP_NADA", "TERCIARIOS");
+		double resultadoTest1 = 1;
+		double resultadoTest2 = -1;
+		double resultadoTest3 = -0.5;
+		double resultadoTest4 = -2;
+		double resultadoTest5 = 1.5;
+		double resultadoTest6 = -1.5;
+		double resultadoTest7 = 2;
+		double resultado;
+		
+		//Escenario 1: (this.puesto== "PRIMARIO")	
+		resultado = testTicketPrimarios.getComparacionEstudios(testTicketPrimarios);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketPrimarios.getComparacionEstudios(testTicketSecundarios);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+		resultado = testTicketPrimarios.getComparacionEstudios(testTicketTerciarios);
+		Assert.assertEquals(resultado, resultadoTest4, 0);
+		
+		//Escenario 2: (this.puesto== "SECUNDARIO")
+		resultado = testTicketSecundarios.getComparacionEstudios(testTicketPrimarios);
+		Assert.assertEquals(resultado, resultadoTest5, 0);
+		resultado = testTicketSecundarios.getComparacionEstudios(testTicketSecundarios);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketSecundarios.getComparacionEstudios(testTicketTerciarios);
+		Assert.assertEquals(resultado, resultadoTest6, 0);
+		
+		//Escenario 3: (this.puesto== "TERCIARIO")		
+		resultado = testTicketTerciarios.getComparacionEstudios(testTicketPrimarios);
+		Assert.assertEquals(resultado, resultadoTest7, 0);
+		resultado = testTicketTerciarios.getComparacionEstudios(testTicketSecundarios);
+		Assert.assertEquals(resultado, resultadoTest5, 0);
+		resultado = testTicketTerciarios.getComparacionEstudios(testTicketTerciarios);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
 	}
 
 	@Test
 	public void testGetComparacionExperiencia() {
-		fail("Not yet implemented");
+		Ticket testTicketExpNada= new Ticket("PRESENCIAL", 1200, "JORNADA_MEDIA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketExpMedia= new Ticket("PRESENCIAL", 1200, "JORNADA_COMPLETA", "SENIOR", "EXP_MEDIA", "SECUNDARIOS");
+		Ticket testTicketExpMucha= new Ticket("PRESENCIAL", 1200, "JORNADA_EXTENDIDA", "MANAGMENT", "EXP_MUCHA", "TERCIARIOS");
+		double resultadoTest1 = 1;
+		double resultadoTest2 = -1;
+		double resultadoTest3 = -0.5;
+		double resultadoTest4 = -2;
+		double resultadoTest5 = 1.5;
+		double resultadoTest6 = -1.5;
+		double resultadoTest7 = 2;
+		double resultado;
+		
+		//Escenario 1: (this.experiencia== "NADA")
+		resultado = testTicketExpNada.getComparacionExperiencia(testTicketExpNada);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketExpNada.getComparacionExperiencia(testTicketExpMedia);
+		Assert.assertEquals(resultado, resultadoTest3, 0);
+		resultado = testTicketExpNada.getComparacionExperiencia(testTicketExpMucha);
+		Assert.assertEquals(resultado, resultadoTest4, 0);
+		//Escenario 2: (this.experiencia== "MEDIA")
+		resultado = testTicketExpMedia.getComparacionExperiencia(testTicketExpNada);
+		Assert.assertEquals(resultado, resultadoTest5, 0);
+		resultado = testTicketExpMedia.getComparacionExperiencia(testTicketExpMedia);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
+		resultado = testTicketExpMedia.getComparacionExperiencia(testTicketExpMucha);
+		Assert.assertEquals(resultado, resultadoTest6, 0);
+		
+		//Escenario 3: (this.experiencia== "MUCHA")			
+		resultado = testTicketExpMucha.getComparacionExperiencia(testTicketExpNada);
+		Assert.assertEquals(resultado, resultadoTest7, 0);
+		resultado = testTicketExpMucha.getComparacionExperiencia(testTicketExpMedia);
+		Assert.assertEquals(resultado, resultadoTest5, 0);
+		resultado = testTicketExpMucha.getComparacionExperiencia(testTicketExpMucha);
+		Assert.assertEquals(resultado, resultadoTest1, 0);
 	}
 
 	@Test
 	public void testGetComparacionRemuneracion() {
+		/*Ticket testTicketr= new Ticket("PRESENCIAL", 1200, "JORNADA_MEDIA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketExpMedia= new Ticket("PRESENCIAL", 1200, "JORNADA_COMPLETA", "SENIOR", "EXP_MEDIA", "SECUNDARIOS");
+		Ticket testTicketExpMucha= new Ticket("PRESENCIAL", 1200, "JORNADA_EXTENDIDA", "MANAGMENT", "EXP_MUCHA", "TERCIARIOS");
+		double resultadoTest1 = 1;
+		double resultadoTest2 = -1;
+		double resultadoTest3 = -0.5;
+		double resultadoTest4 = -2;
+		double resultadoTest5 = 1.5;
+		double resultadoTest6 = -1.5;
+		double resultadoTest7 = 2;
+		double resultado;*/
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetComparacionTotal() {
-		fail("Not yet implemented");
+		Ticket testTicketTotal1= new Ticket("PRESENCIAL", 1200, "JORNADA_MEDIA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		Ticket testTicketTotal2= new Ticket("PRESENCIAL", 1200, "JORNADA_MEDIA", "JUNIOR", "EXP_NADA", "PRIMARIOS");
+		double resultadoEsperado = 6;
+		double resultado = testTicketTotal1.getComparacionTotal(testTicketTotal2);
+		Assert.assertEquals(resultado, resultadoEsperado, 0);
+
 	}
 
 }
