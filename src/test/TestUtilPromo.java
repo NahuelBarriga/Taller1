d58a4711f0279f4ac52a15f3a0d93a0b8b30f044
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-11-04 18:48:00 -0300

    Create TestUtilPromo.java

---
+++ src/test/TestUtilPromo.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUtilPromo {
+
+	@Test
+	public void testAplicaPromo() {
+		fail("Not yet implemented");
+	}
+
+}
